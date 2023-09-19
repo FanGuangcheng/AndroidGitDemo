9006c46a498ba42497bc106f48c8b987701e70f8
fanguangcheng <mengfeicheng2012@163.com>
2023-09-19 10:57:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidgitdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidgitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // test git rebase和merge
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
