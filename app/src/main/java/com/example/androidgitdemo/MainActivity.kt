a22afd4a811ade8e98d8c0fd19c229589a0f9e5d
fanguangcheng <mengfeicheng2012@163.com>
2023-09-19 11:00:36 +0800

    first commint on main branch

--- app/src/main/java/com/example/androidgitdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidgitdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     // test git rebase和merge
+    // first commit  on main branch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
