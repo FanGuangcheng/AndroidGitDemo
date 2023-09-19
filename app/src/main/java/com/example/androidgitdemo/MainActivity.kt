aa4ce51d04fa90784affa9bb937a14b15d1c70dd
fanguangcheng <mengfeicheng2012@163.com>
2023-09-19 11:31:59 +0800

    main branch commint 11111

--- app/src/main/java/com/example/androidgitdemo/MainActivity.kt
+++ app/src/main/java/com/example/androidgitdemo/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     // test git rebase和merge
     // first commit  on main branch
     // second commit  on main branch
-    
+
+    // main branch commint 111111
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
